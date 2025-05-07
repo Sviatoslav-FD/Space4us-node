5e72246187adf0bb1603dcb27fa28e8c746c9d24
Sviatoslav-FD <sviatoslavpanevnyk@gmail.com>
2025-05-07 13:13:57 +0300

    WIP

--- routes/countries.js
+++ routes/countries.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const fs = require('fs')
 
 router.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, '..', 'countries.json'), 'utf8', function readFileCallback(error, data) {
+    fs.readFile(express.static(path.join(__dirname, '..', 'countries.json')), 'utf8', function readFileCallback(error, data) {
         if (error) {
             res.json(error)
             return;
