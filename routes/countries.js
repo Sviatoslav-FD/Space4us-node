a3a37ec6a2e9de51afbefd3a0f95f2c196091f2d
Sviatoslav-FD <sviatoslavpanevnyk@gmail.com>
2025-05-07 12:57:36 +0300

    WIP

--- routes/countries.js
+++ routes/countries.js
@@ -3,15 +3,30 @@ const router = express.Router()
 const fs = require('fs')
 
 router.get('/', (req, res) => {
-    fs.readFile('/countries.json', 'utf8', function readFileCallback(err, data) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(data);
+    // fs.readFile('countries.json', 'utf8', function readFileCallback(err, data) {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    //     console.log(data);
         
-        res.json(data)
-    });
+    //     res.json(data)
+    // });
+
+    res.json([
+        {
+            "flag": "United States",
+            "code": "US"
+        },
+        {
+            "flag": "Canada",
+            "code": "CA"
+        },
+        {
+            "flag": "Mexico",
+            "code": "MX"
+        }
+    ])
 })
 
 module.exports = router
