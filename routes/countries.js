b0f4523150292619b0c8669603f29836b83137a1
Sviatoslav-FD <sviatoslavpanevnyk@gmail.com>
2025-05-07 13:20:43 +0300

    WIP

--- routes/countries.js
+++ routes/countries.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 
 router.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, 'countries.json'), 'utf8', function readFileCallback(error, data) {
+    fs.readFile(path.join(__dirname, '..', 'countries.json'), 'utf8', function readFileCallback(error, data) {
         if (error) {
             res.json(error)
             return;
